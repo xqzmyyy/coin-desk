0e9fe6ddc29b59c4e68080d7105d13c608145a50
Kamila Koval <kamila-koval@MacBook-Pro-Kamila.local>
2024-08-30 08:55:40 +0300

    account page mockup

---
+++ app/(tabs)/price.tsx
@@ -0,0 +1,26 @@
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+
+export default function priceScreen() {
+    return (
+        <View>
+            {/* <StatusBar
+                barStyle="default"
+                translucent={true}
+                backgroundColor="transparent"
+            /> */}
+            <Text>Price page</Text>
+        </View>
+    )
+}
+
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         paddingTop: StatusBar.currentHeight, 
+//         paddingHorizontal: 16,
+//       },
+//       text: {
+//         fontSize: 20,
+//       },
+// })
