91f5534f4fa8950019a24ba2eba1f5efbf2b938d
Kamila Koval <kamila-koval@MacBook-Pro-Kamila.local>
2024-09-08 11:40:36 +0300

    calendar component && price page layout

--- app/(tabs)/price.tsx
+++ app/(tabs)/price.tsx
@@ -1,11 +1,49 @@
 import { CurrentPrice } from "@/components/price/CurrentPrice"
-import { Text, View } from "react-native"
+import { HistoricalPrice } from "@/components/price/HistoricalPrice"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function priceScreen() {
     return (
-        <View>
-            <Text>Price page</Text>
-            <CurrentPrice/>
+        <View style={styles.pageCont}>
+            <View>
+                <CurrentPrice/>
+            </View>
+            <View style={styles.historicalSection}>
+                <View style={styles.historicalContCalendar}>
+                    <HistoricalPrice/>
+                </View>
+                <View style={styles.historicalCont}>
+                    <Text>test text</Text>
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    pageCont: {
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center'
+    },
+
+    historicalSection: {
+        padding: 12,
+        width: '100%'
+    },
+
+    historicalContCalendar: {
+        backgroundColor: '#B7A7AE',
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12,
+        padding: 12
+    },
+
+    historicalCont: {
+        backgroundColor: '#E1D5D9',
+        padding: 12,
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
+        height: 200 // TEST DELETE THIS
+    },
+})
